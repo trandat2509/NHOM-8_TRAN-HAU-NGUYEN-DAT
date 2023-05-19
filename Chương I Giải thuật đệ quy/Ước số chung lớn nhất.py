86f22c58ead314495103cb8b933f4b5a1bc24089
trandat2509 <120194665+trandat2509@users.noreply.github.com>
2023-05-19 22:55:39 +0700

    Delete Chương I Giải thuật đệ quy directory

--- Chương I Giải thuật đệ quy/Ước số chung lớn nhất.py
+++
@@ -1,9 +0,0 @@
-def uscln(a, b):
-    if (b == 0):
-        return a
-    return uscln(b, a % b)
-
-x = int(input("Nhập số nguyên dương a: "))
-y = int(input("Nhập số nguyên dương b: "))
-print("Ước số chung lớn nhất của", x, "và", y, "là:", uscln(x, y))
-
